e90cb14417a97943bf8cc416625bcd0ac2a5305a
Ilya Vladimirskiy <ilya@ilyas-mbp.intern>
2024-11-03 16:10:06 +0100

    minor changes

---
+++ etl/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+
+HOST="34.32.52.216"
+PORT="5432"
+DBNAME="as_stats"
+USER="as_stats"
+PASSWORD='2Z£D9!6X(j|sHI2Sv$rW'
+
+def get_db_connection(password):
+    connection_string = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
+    engine = create_engine(connection_string)
+    return engine.connect()
+
+def insert_all_asns_for_country(country_iso2, list_of_asns):
+    connection = get_db_connection(PASSWORD)
+    sql= "insert into asn_country (a_country_iso2, a_ripe_id) values ('aa', 100)"
+    connection.execute(sql)
