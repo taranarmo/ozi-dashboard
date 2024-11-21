d7237f42b387eada9616917e7bcce2ffd75093be
Ilya <bkmy43@googlemail.com>
2024-11-21 22:54:51 +0100

    fixed timestamp, moved tables to putlic schema

--- etl/main.py
+++ etl/main.py
@@ -8,7 +8,7 @@ def get_list_of_asns_for_country(country_iso2):
     print(f"Getting ASNs", end=" ... ")
     d = get_country_resource_list(country_iso2, copy_to_file=True)
     asn_list = d['data']['resources']['asn']
-    print("{} found".format(len(asn_list)))
+    print(f"{len(asn_list)} found")
     return asn_list
 
 
@@ -16,7 +16,7 @@ def get_stats_for_country(country_iso2, date_from, date_to, resolution):
     print("Getting historical stats", end=' ... ')
     d = get_country_resource_stats(country_iso2, resolution, date_from, date_to, copy_to_file=True)
     stats = d['data']['stats']
-    print("stats for {} days found".format(len(stats)))
+    print(f"stats for {len(stats)} days found")
     return stats
 
 
@@ -26,14 +26,14 @@ def main():
         asns = get_list_of_asns_for_country(iso2)
         insert_country_asns_to_db(iso2, asns, True)
 
-        print(f"getting 1d stats for {iso2}".format(iso2=iso2))
+        print(f"Getting 1d stats for {iso2}")
         stats = get_stats_for_country(iso2, '2014-01-01', '2025-01-01', '1d')
         insert_country_stats_to_db(iso2, '1d', stats, True)
 
         for year in range(2019, 2025):
-            print("getting stats for year {}".format(year))
-            date_from = f"{year}-01-01".format(year=year)
-            date_to = f'{year + 1}-01-01'.format(year=year)
+            print(f"Getting 5m stats for {iso2} year {year}")
+            date_from = f"{year}-01-01"
+            date_to = f'{year + 1}-01-01'
 
             stats = get_stats_for_country(iso2, date_from, date_to, '5m')
             insert_country_stats_to_db(iso2, '5m', stats, True)
