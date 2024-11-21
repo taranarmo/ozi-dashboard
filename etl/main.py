44f3f6130344e210ebede80b96921076747196a3
Ilya <bkmy43@googlemail.com>
2024-11-21 21:46:29 +0100

    new courntry list

--- etl/main.py
+++ etl/main.py
@@ -1,11 +1,6 @@
 from database import insert_country_asns_to_db, insert_country_stats_to_db
+from country_lists import EX_SOVIET_COUNTRIES, REPORT_COUNTRIES
 from ripe_api import get_country_resource_list, get_country_resource_stats
-
-EX_SOVIET_COUNTRIES = {
-    "AM": "Armenia", "AZ": "Azerbaijan", "BY": "Belarus", "EE": "Estonia", "GE": "Georgia", "KZ": "Kazakhstan",
-    "KG": "Kyrgyzstan", "LV": "Latvia", "LT": "Lithuania", "MD": "Moldova", "RU": "Russia", "TJ": "Tajikistan",
-    "TM": "Turkmenistan", "UA": "Ukraine", "UZ": "Uzbekistan"
-}
 
 STATS_RESOLUTION = '1d'
 
@@ -26,8 +21,8 @@ def get_stats_for_country(country_iso2, date_from, date_to, resolution):
 
 
 def main():
-    for iso2 in EX_SOVIET_COUNTRIES:
-        print(f"\n{iso2} - {EX_SOVIET_COUNTRIES[iso2]}")
+    for iso2 in REPORT_COUNTRIES:
+        print(f"\n{iso2} - {REPORT_COUNTRIES[iso2]}")
         asns = get_list_of_asns_for_country(iso2)
         insert_country_asns_to_db(iso2, asns, True)
 
