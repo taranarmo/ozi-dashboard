a18f09c63e8bcb30789b589a032c852d2b6cefe5
Your Name <ilya@lab4.berlin>
2025-02-11 11:47:34 +0000

    testing commits

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 export DBUSER=ozi_dashboard
 export DBNAME=ozi_dashboard
 export DBHOST=ozi-dashboard-db2.cm7ygcoo08e0.us-east-1.rds.amazonaws.com
+
 echo "Please enter the database passsword (user postgres)"
 read -s PGPASSWORD
 export PGPASSWORD=$PGPASSWORD
