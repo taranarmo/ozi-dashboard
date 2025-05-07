703e4a1055253cce1ac5ed603d5cac8a81f7bf98
Ilya <bkmy43@googlemail.com>
2025-05-07 11:02:20 +0200

    resolution param was ignored - fixed

--- etl/main.py
+++ etl/main.py
@@ -94,7 +94,7 @@ def generate_dates(date_from, date_to, resolution):
 
 def etl_load_asns(iso2, date_from, date_to, resolution):
     print(f"{' '*12}Getting data from the API and storing to DB... ")
-    dates = generate_dates(date_from, date_to, 'D')
+    dates = generate_dates(date_from, date_to, resolution)
     for asns_batch in get_list_of_asns_for_country(iso2, dates, BATCH_SIZE):
         insert_country_asns_to_db(iso2, asns_batch, save_sql_to_file=False, load_to_database=True)
 
