44f9126ffe19a2e96ecc90cde409e4856c62221c
Ilya <bkmy43@googlemail.com>
2025-05-07 11:20:17 +0200

    tasks to get list of dates and not from, to, resolution anymore

--- etl/main.py
+++ etl/main.py
@@ -58,8 +58,8 @@ def main():
         print(f"Date To:    {date_to.strftime("%Y-%m-%d")}")
         print(f"Resolution: {RESOLUTION_DICT[resolution]}")
 
-        # task_map[task](iso2, generate_dates(date_from, date_to, resolution))
-        task_map[task](iso2, date_from, date_to, resolution)
+        task_map[task](iso2, generate_dates(date_from, date_to, resolution))
+        # task_map[task](iso2, date_from, date_to, resolution)
 
         print(f"\nAt:         {datetime.now()}")
         print(f"Finished:   {task}")
@@ -92,9 +92,8 @@ def generate_dates(date_from, date_to, resolution):
     return dates
 
 
-def etl_load_asns(iso2, date_from, date_to, resolution):
+def etl_load_asns(iso2, dates):
     print(f"{' '*12}Getting data from the API and storing to DB... ")
-    dates = generate_dates(date_from, date_to, resolution)
     for asns_batch in get_list_of_asns_for_country(iso2, dates, BATCH_SIZE):
         insert_country_asns_to_db(iso2, asns_batch, save_sql_to_file=False, load_to_database=True)
 
