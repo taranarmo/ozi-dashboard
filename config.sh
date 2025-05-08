2a42d171e577dd0437f8111229c5681129aa503c
Ilya <bkmy43@googlemail.com>
2025-05-09 00:40:47 +0200

    path fixed again

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 export OZI_DATABASE_USER=ozi
-export OZI_DATABASE_PASSWORD=Jdczyrf#999
 export OZI_DATABASE_NAME=ozi_db
 export OZI_DATABASE_PORT=5432
 export OZI_DATABASE_HOST=localhost
 
-echo "Please enter the database passsword (user ozi)"
+echo "Please enter the database password (user $OZI_DATABASE_USER):"
 read -s OZI_DATABASE_PASSWORD
-export PGPASSWORD=OZI_DATABASE_PASSWORD
+export OZI_DATABASE_PASSWORD
+export PGPASSWORD=$OZI_DATABASE_PASSWORD
 
 source venv/bin/activate
